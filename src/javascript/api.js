88f25d63741808fba70fc7b524801a63e3738720
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-22 22:33:58 +0300

    update condition

--- src/javascript/api.js
+++ src/javascript/api.js
@@ -14,48 +14,46 @@ const API = () => {
     .catch((error) => {
       handleFetchError(error);
     });
+};
 
-  const createCountryCard = (countries) => {
-    if (countries.length === 0) {
-      return;
-    }
+const createCountryCard = (countries) => {
+  if (!Array.isArray(countries) || countries.length === 0) {
+    return;
+  }
 
-    console.log(countries);
-    const cardsWrapper = document.getElementById("cards");
-    countries.forEach((country) => {
-      const card = document.createElement("div");
-      card.className =
-        "rounded-xl bg-gray-800 text-white cursor-pointer shadow-xl country-card";
-      const imageContainer = document.createElement("div");
-      imageContainer.className = "overflow-hidden";
-      const countryFlag = document.createElement("img");
-      countryFlag.src = country.flags.png;
-      countryFlag.alt = `${country.name.common} flag`;
-      countryFlag.className =
-        "w-full h-64 object-cover rounded-t-xl hover:scale-100";
-      imageContainer.appendChild(countryFlag);
+  console.log(countries);
+  const cardsWrapper = document.getElementById("cards");
+  countries.forEach((country) => {
+    const card = document.createElement("div");
+    card.className =
+      "rounded-xl bg-gray-800 text-white cursor-pointer shadow-xl country-card";
+    const imageContainer = document.createElement("div");
+    imageContainer.className = "overflow-hidden";
+    const countryFlag = document.createElement("img");
+    countryFlag.src = country.flags.png;
+    countryFlag.alt = `${country.name.common} flag`;
+    countryFlag.className =
+      "w-full h-64 object-cover rounded-t-xl hover:scale-100";
+    imageContainer.appendChild(countryFlag);
 
-      //! Country details
+    const countryContent = document.createElement("div");
+    countryContent.className = "mt-4 flex flex-col items-center gap-6 p-2";
+    const countryName = document.createElement("h1");
+    countryName.className = "text-2xl";
+    countryName.textContent = country.name.common;
+    const countryDetails = document.createElement("div");
+    countryDetails.className = "flex flex-col items-center";
+    countryDetails.innerHTML = `
+  <span>Population: ${country.population}</span>
+  <p>Capital: ${country.capital}</p>
+  `;
+    countryContent.appendChild(countryName);
+    countryContent.appendChild(countryDetails);
 
-      const countryContent = document.createElement("div");
-      countryContent.className = "mt-4 flex flex-col items-center gap-6 p-2";
-      const countryName = document.createElement("h1");
-      countryName.className = "text-2xl";
-      countryName.textContent = country.name.common;
-      const countryDetails = document.createElement("div");
-      countryDetails.className = "flex flex-col items-center";
-      countryDetails.innerHTML = `
-    <span>Population: ${country.population}</span>
-    <p>Capital: ${country.capital}</p>
-    `;
-      countryContent.appendChild(countryName);
-      countryContent.appendChild(countryDetails);
-
-      card.appendChild(imageContainer);
-      card.appendChild(countryContent);
-      cardsWrapper.appendChild(card);
-    });
-  };
+    card.appendChild(imageContainer);
+    card.appendChild(countryContent);
+    cardsWrapper.appendChild(card);
+  });
 };
 
 const handleFetchError = (error) => {
