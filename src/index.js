3fadec7606e193d636aae90ae909155a9a56f14e
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-22 21:22:28 +0300

    updated country content and details

--- src/index.js
+++ src/index.js
@@ -15,11 +15,12 @@ const createCountryCard = (countries) => {
     card.className =
       "rounded-xl bg-gray-800 text-white cursor-pointer shadow-xl";
     const imageContainer = document.createElement("div");
-    imageContainer.className = "overflow-hidden"
+    imageContainer.className = "overflow-hidden";
     const countryFlag = document.createElement("img");
     countryFlag.src = country.flags.png;
     countryFlag.alt = `${country.name.common} flag`;
-    countryFlag.className = "w-full h-64 object-cover rounded-t-xl hover:scale-100";
+    countryFlag.className =
+      "w-full h-64 object-cover rounded-t-xl hover:scale-100";
     imageContainer.appendChild(countryFlag);
 
     //! Country details
@@ -27,9 +28,10 @@ const createCountryCard = (countries) => {
     const countryContent = document.createElement("div");
     countryContent.className = "mt-4 flex flex-col items-center gap-6 p-2";
     const countryName = document.createElement("h1");
-    countryName.className = "text-xl";
+    countryName.className = "text-2xl";
     countryName.textContent = country.name.common;
     const countryDetails = document.createElement("div");
+    countryDetails.className = "flex flex-col items-center";
     countryDetails.innerHTML = `
     <span>Population: ${country.population}</span>
     <p>Capital: ${country.capital}</p>
