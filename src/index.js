e947d4580e7c748a8899cda53f35e1ac168e0ad9
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-22 20:24:27 +0300

    new commit

--- src/index.js
+++ src/index.js
@@ -7,4 +7,13 @@ fetch(API_URL)
   })
   .catch((error) => console.error("Error fetching data", error));
 
+const createCountryCard = (countries) => {
+  console.log(countries);
+  const cardsWrapper = document.getElementById("cards");
+  countries.forEach((country) => {
+    const card = document.createElement("div");
+    card.className = "rounded-xl p-5 bg-gray-800 text-white cursor-pointer";
+    cardsWrapper.appendChild(card);
+  });
 
+};
