4c2307e130124854b3d9bec18267856584b9c4af
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-22 20:58:10 +0300

    new commit

--- src/index.js
+++ src/index.js
@@ -15,16 +15,17 @@ const createCountryCard = (countries) => {
     card.className =
       "rounded-xl bg-gray-800 text-white cursor-pointer shadow-xl";
     const imageContainer = document.createElement("div");
+    imageContainer.className = "overflow-hidden"
     const countryFlag = document.createElement("img");
     countryFlag.src = country.flags.png;
     countryFlag.alt = `${country.name.common} flag`;
-    countryFlag.className = "w-full h-32 object-cover rounded-t-xl";
+    countryFlag.className = "w-full h-64 object-cover rounded-t-xl hover:scale-100";
     imageContainer.appendChild(countryFlag);
 
     //! Country details
 
     const countryContent = document.createElement("div");
-    countryContent.className = "mt-4 flex flex-col items-center gap-6";
+    countryContent.className = "mt-4 flex flex-col items-center gap-6 p-2";
     const countryName = document.createElement("h1");
     countryName.className = "text-xl";
     countryName.textContent = country.name.common;
