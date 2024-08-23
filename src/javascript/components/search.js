352bc784d08e9303be8d66e2bccf33921a1418c1
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-23 11:02:29 +0300

    live search

--- src/javascript/components/search.js
+++ src/javascript/components/search.js
@@ -1,5 +1,9 @@
 const Search = () => {
-    
+  const countryInput = document.getElementById("country-input");
+  countryInput.addEventListener("input", () => {
+    const searchValue = countryInput.value.toLowerCase();
+    const countryCards = document.querySelectorAll(".country-card")
+  });
 };
 
 export default Search;
