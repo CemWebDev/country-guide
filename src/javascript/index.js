30de14dc52c9e1be75d8b7d7f9254997cb4ab9c2
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-23 18:39:56 +0300

    update index.js

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,4 +1,4 @@
-import API from "./components/api.js";
+import { API } from "./components/api.js";
 import Search from "./components/search.js";
 import PreventDefaultForm from "./components/preventDefaultForm.js";
 import StickyForm from "./components/stickyForm.js";
