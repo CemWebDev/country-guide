c8c0ac5f8021e474915c585d4c5606a2d359345e
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-23 11:15:46 +0300

    card count variable

--- src/javascript/components/search.js
+++ src/javascript/components/search.js
@@ -1,19 +1,25 @@
+let cardCount = 250;
+
 const Search = () => {
   const countryInput = document.getElementById("country-input");
+
   countryInput.addEventListener("input", () => {
     const searchValue = countryInput.value.toLowerCase();
     const countryCards = document.querySelectorAll(".country-card");
 
+    cardCount = 0;
     countryCards.forEach((card) => {
       const countryName = card.querySelector("h1").textContent.toLowerCase();
       if (countryName.includes(searchValue)) {
         card.classList.remove("hidden");
         card.classList.add("block");
+        cardCount++;
       } else {
         card.classList.remove("block");
         card.classList.add("hidden");
       }
     });
+    console.log(cardCount);
   });
 };
 
