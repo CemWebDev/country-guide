3bcd0ab2c9680bea9d0af970c5dbb8f9abd85199
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-23 18:19:53 +0300

    asynchronous function

--- src/javascript/components/api.js
+++ src/javascript/components/api.js
@@ -1,25 +1,21 @@
 import { createCountryCard } from "../utils/createCountryCard.js";
 import { handleFetchError } from "../utils/handleFetchError.js";
 
-let allCountries = [];
+export let allCountries = [];
 
-const API = () => {
+export const API = async () => {
   const API_URL = "https://restcountries.com/v3.1/all";
 
-  fetch(API_URL)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      allCountries = data;
-      createCountryCard(data);
-    })
-    .catch((error) => {
-      handleFetchError(error);
-    });
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status ${response.status}`);
+    }
+    const data = await response.json();
+    allCountries = data;
+    createCountryCard(data);
+    return allCountries;
+  } catch (error) {
+    handleFetchError(error);
+  }
 };
-
-export default API;
