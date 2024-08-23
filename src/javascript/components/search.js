d55764707a760b7246107af38ab4838693b9b268
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-23 11:07:37 +0300

    search

--- src/javascript/components/search.js
+++ src/javascript/components/search.js
@@ -2,7 +2,18 @@ const Search = () => {
   const countryInput = document.getElementById("country-input");
   countryInput.addEventListener("input", () => {
     const searchValue = countryInput.value.toLowerCase();
-    const countryCards = document.querySelectorAll(".country-card")
+    const countryCards = document.querySelectorAll(".country-card");
+
+    countryCards.forEach((card) => {
+      const countryName = card.querySelector("h1").textContent.toLowerCase();
+      if (countryName.includes(searchValue)) {
+        card.classList.remove("hidden");
+        card.classList.add("block");
+      } else {
+        card.classList.remove("block");
+        card.classList.add("hidden");
+      }
+    });
   });
 };
 
